a796d5209966bfb61d89c3ba03864e10abf4e328
Michal Jankovic <xjankov@fi.muni.cz>
2020-08-09 23:48:49 +0200

    imgui done?

--- src/glpp/imgui/imgui.cpp
+++ src/glpp/imgui/imgui.cpp
@@ -1,5 +1,49 @@
-//
-// Created by miso on 8/7/20.
-//
+#include "glpp/imgui/imgui.hpp"
 
-#include "imgui.hpp"
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+namespace glpp::imgui
+{
+    ImGui::ImGui(glpp::glfw::Window& window)
+      : window_{window}
+    {
+        ::IMGUI_CHECKVERSION();
+        ::ImGui::CreateContext();
+        ::ImGui_ImplGlfw_InitForOpenGL(window.api_ptr(), false);
+        ::ImGui_ImplOpenGL3_Init();
+
+        mouse_button_cb_conn_ = window.api_event_context().on_api_mouse_button([this](auto const... args) {
+            ::ImGui_ImplGlfw_MouseButtonCallback(window_.api_ptr(), args...);
+        });
+        scroll_cb_conn_ = window.api_event_context().on_api_scroll([this](auto const... args) {
+            ::ImGui_ImplGlfw_ScrollCallback(window_.api_ptr(), args...);
+        });
+        key_cb_conn_ = window.api_event_context().on_api_key([this](auto const... args) {
+            ::ImGui_ImplGlfw_KeyCallback(window_.api_ptr(), args...);
+        });
+        char_cb_conn_ = window.api_event_context().on_api_char([this](auto const... args) {
+            ::ImGui_ImplGlfw_CharCallback(window_.api_ptr(), args...);
+        });
+    }
+
+    ImGui::~ImGui() noexcept
+    {
+        ::ImGui_ImplOpenGL3_Shutdown();
+        ::ImGui_ImplGlfw_Shutdown();
+        ::ImGui::DestroyContext();
+    }
+
+    void ImGui::new_frame()
+    {
+        ::ImGui_ImplOpenGL3_NewFrame();
+        ::ImGui_ImplGlfw_NewFrame();
+        ::ImGui::NewFrame();
+    }
+
+    void ImGui::render()
+    {
+        ::ImGui::Render();
+        ::ImGui_ImplOpenGL3_RenderDrawData(::ImGui::GetDrawData());
+    }
+}  // namespace glpp::imgui
--- src/glpp/imgui/imgui.hpp
+++ src/glpp/imgui/imgui.hpp
@@ -1,8 +1,33 @@
 #pragma once
 
+#include <boost/signals2.hpp>
+#include <imgui.h>
+#include "glpp/glfw/window.hpp"
+
 namespace glpp::imgui
 {
     class ImGui
     {
+      public:
+        explicit ImGui(glfw::Window& window);
+
+        ImGui(ImGui const&) = delete;
+        ImGui(ImGui&&) = delete;
+
+        ~ImGui() noexcept;
+
+        auto operator=(ImGui const&) -> ImGui& = delete;
+        auto operator=(ImGui&&) -> ImGui& = delete;
+
+        void new_frame();
+
+        void render();
+
+      private:
+        glfw::Window& window_;
+        boost::signals2::scoped_connection mouse_button_cb_conn_;
+        boost::signals2::scoped_connection scroll_cb_conn_;
+        boost::signals2::scoped_connection key_cb_conn_;
+        boost::signals2::scoped_connection char_cb_conn_;
     };
 }  // namespace glpp::imgui
