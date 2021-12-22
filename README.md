371d5339769358f73a433df01c3b18bbd5411f67
Michal Jankovič <michal.jankovic@sanezoo.com>
2021-12-22 14:07:11 +0100

    Switch to new transparent conan integration

--- README.md
+++ README.md
@@ -2,29 +2,12 @@
 
 C++ wrapper library for OpenGL and GLFW, with ImGui integration support. 
 
-## Dependencies
+## Installation
 
-The Conan and Vcpkg package managers are supported for installing dependencies.
+Recommended approach is using conan:
 
-### Using Conan
+```sh
+conan create .
+```
 
-Simply create the project in your cache, conan will take care of the rest.
-
-### Using Vcpkg
-
-The following packages must be installed:
-
-* fmt
-* glad
-* glm
-* magic-enum
-* ms-gsl
-* With BUILD_CONFIG:
-	* nlohmann-json
-* With BUILD_GLFW:
-	* boost-signals2
-	* glfw3
-* With BUILD_IMGUI:
-	* imgui\[core,glfw-binding,opengl3-glad-binding\]
-* With BUILD_EXAMPLES:
-	* clara
+Then add `glpp/0.2.0` to your project requirements.
--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
 
 
 class Glpp(ConanFile):
@@ -13,20 +14,19 @@ class Glpp(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "glfw": [True, False],
-        "config": [True, False],
-        "imgui": [True, False],
-        "examples": [True, False],
+        "with_glfw": [True, False],
+        "with_config": [True, False],
+        "with_imgui": [True, False],
+        "with_examples": [True, False],
     }
     default_options = {
         "fPIC": True,
-        "glfw": True,
-        "config": True,
-        "imgui": True,
-        "examples": True,
+        "with_glfw": True,
+        "with_config": True,
+        "with_imgui": True,
+        "with_examples": True,
         "glad:gl_version": "4.5",
     }
-    generators = "cmake"
     exports_sources = (
         "src/*",
         "examples/*",
@@ -38,65 +38,76 @@ class Glpp(ConanFile):
         "glm/0.9.9.8",
         "magic_enum/0.7.3",
         "ms-gsl/3.0.1",
+        "opengl/system",
     )
 
     def requirements(self):
-        if self.options.glfw:
-            self.requires("boost/1.76.0")
+        if self.options.with_glfw:
+            self.requires("boost/1.77.0")
             self.requires("glfw/3.3.2")
 
-        if self.options.config:
+        if self.options.with_config:
             self.requires("nlohmann_json/3.9.1")
 
-        if self.options.imgui:
+        if self.options.with_imgui:
             self.requires("imgui/1.77")
-
-        if self.options.examples:
+    
+        if self.options.with_examples:
             self.requires("lyra/1.5.1")
 
     def imports(self):
-        # Collect shared libraries
         self.copy("*.so*", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
 
-        if self.options.imgui:
-            # Collect ImGui bindings
+        if self.options.with_imgui:
             self.copy("imgui_impl_glfw.*", dst="bindings", src="res/bindings")
             self.copy("imgui_impl_opengl3.*", dst="bindings", src="res/bindings")
 
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_EXAMPLES"] = self.options.with_examples
+        tc.variables["BUILD_CONFIG"] = self.options.with_config
+        tc.variables["BUILD_GLFW"] = self.options.with_glfw
+        tc.variables["BUILD_IMGUI"] = self.options.with_imgui
+        tc.generate()
+
     def build(self):
         cmake = CMake(self)
-
-        cmake.definitions["BUILD_GLFW"] = self.options.glfw
-        cmake.definitions["BUILD_CONFIG"] = self.options.config
-        cmake.definitions["BUILD_EXAMPLES"] = self.options.examples
-
         cmake.configure()
         cmake.build()
 
     def package(self):
-        header_excludes = []
-
-        if not self.options.glfw:
-            header_excludes.append("glpp/glfw/*")
-        if not self.options.config:
-            header_excludes.append("glpp/config/*")
-
-        self.copy("*.hpp", dst="include", src="src", excludes=tuple(header_excludes))
-        self.copy("*.so*", dst="lib", keep_path=False, symlinks=True)
-        self.copy("*.a", dst="lib", keep_path=False)
+        cmake = CMake(self)
+        cmake.install()
 
     def package_id(self):
-        # Does not impact the resulting package, exclude from package id
-        del self.info.options.examples
+        del self.info.options.with_examples
 
     def package_info(self):
-        if self.options.imgui:
-            self.cpp_info.libs.append("glpp_imgui")
+        self.cpp_info.components["core"].libs = ["glpp_core"]
+        self.cpp_info.components["core"].requires = [
+            "fmt::fmt",
+            "glad::glad",
+            "glm::glm",
+            "opengl::opengl",
+            "magic_enum::magic_enum",
+            "ms-gsl::ms-gsl",
+        ]
+        
+        if self.options.with_glfw:
+            self.cpp_info.components["glfw"].libs = ["glpp_glfw"]
+            self.cpp_info.components["glfw"].requires = ["boost::boost", "glfw::glfw", "core"]
 
-        if self.options.glfw:
-            self.cpp_info.libs.append("glpp_glfw")
+        if self.options.with_config:
+            self.cpp_info.components["config"].libs = ["glpp_config"]
+            self.cpp_info.components["config"].requires = ["nlohmann_json::nlohmann_json", "core"]
 
-        if self.options.config:
-            self.cpp_info.libs.append("glpp_config")
+        if self.options.with_imgui:
+            self.cpp_info.components["imgui"].libs = ["glpp_imgui"]
+            self.cpp_info.components["imgui"].requires = ["imgui::imgui", "core", "glfw"]
 
-        self.cpp_info.libs.append("glpp")
+        if self.options.with_examples:
+            self.cpp_info.components["examples"].requires = ["lyra::lyra"]
